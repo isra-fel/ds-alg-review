1baee95843b48d1b11f4a7b4ac95a06281730036
Yeming Liu <Yeming.Liu@microsoft.com>
2019-07-24 16:07:59 +0800

    jest reporter

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testResultsProcessor: './node_modules/jest-junit-reporter'
 };
